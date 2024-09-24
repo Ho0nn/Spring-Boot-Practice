f224ad27f4137a45695bafd532d2ca0cc7ccd78e
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-09-24 05:12:05 +0300

    Actuator / Security / Custom App Properties

--- src/main/java/com/luve2code/luv2code/rest/FunRestController.java
+++ src/main/java/com/luve2code/luv2code/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package com.luve2code.luv2code.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // injecting properties from application.properties
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Team Name: " + teamName + " &  Coaches Name: " + coachName;
+    }
+
     @GetMapping("/")
     public String hello() {
         return "Hello World!";
